b3e23eb6b8ea85d5554089ecf249168ea9d74f8e
Aniruddha Gedam <gedamaniruddha@gmail.com>
2025-03-20 12:30:49 +0530

    Update wsgi.py to use absolute path for backend directory

--- wsgi.py
+++ wsgi.py
@@ -2,7 +2,7 @@ import sys
 import os
 
 # Add the backend directory to the Python path
-sys.path.insert(0, os.path.abspath('backend'))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), 'backend')))
 
 # Import the Flask application from the backend directory
 from app import app
